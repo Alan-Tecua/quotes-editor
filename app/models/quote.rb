741bd2b7bdb4d038a06a5ce7da322ee623bab99c
alan tecuapetla <alan.tecuapetla@gmail.com>
2023-09-14 16:28:01 +0900

    added update and remove bradcast in quote model

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -3,6 +3,8 @@ class Quote < ApplicationRecord
   scope :ordered, -> { order(id: :desc) }
   # after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self } }
   after_create_commit -> { broadcast_prepend_to "quotes" }
+  after_update_commit -> { broadcast_replace_to "quotes" }
+  after_destroy_commit -> { broadcast_remove_to "quotes" }
 
   # Ex:- scope :active, -> {where(:active => true)}
 end
