c2117b185da6ba5a8817662e2302f92a3fadd836
alan tecuapetla <alan.tecuapetla@gmail.com>
2023-09-14 16:45:07 +0900

    changed the broadcast to one line, tested later and not broken

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,9 +2,13 @@ class Quote < ApplicationRecord
   validates :name, presence: true
   scope :ordered, -> { order(id: :desc) }
   # after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self } }
-  after_create_commit -> { broadcast_prepend_to "quotes" }
-  after_update_commit -> { broadcast_replace_to "quotes" }
-  after_destroy_commit -> { broadcast_remove_to "quotes" }
+
+  # after_create_commit -> { broadcast_prepend_later_to "quotes" }
+  # after_update_commit -> { broadcast_replace_later_to "quotes" }
+  # after_destroy_commit -> { broadcast_remove_to "quotes" }
+
+  # this line bellow is equivelant to the lines above
+  broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
 
   # Ex:- scope :active, -> {where(:active => true)}
 end
