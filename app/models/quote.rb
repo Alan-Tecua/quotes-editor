24e9942c1aa326ef958819fbf828fa8d0c4532a9
alan tecuapetla <alan.tecuapetla@gmail.com>
2023-09-14 16:16:14 +0900

    added some css cnages and the redis gem

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,5 +1,8 @@
 class Quote < ApplicationRecord
   validates :name, presence: true
   scope :ordered, -> { order(id: :desc) }
+  # after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self } }
+  after_create_commit -> { broadcast_prepend_to "quotes" }
+
   # Ex:- scope :active, -> {where(:active => true)}
 end
